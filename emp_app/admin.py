a974ba37c72821571ec678c17934b40e32f839c8
VaishaliJadhav <vaishali.jadhav3007@gmail.com>
2022-09-02 23:17:19 +0530

    firstcommit

---
+++ emp_app/admin.py
@@ -0,0 +1,10 @@
+from curses.ascii import EM
+from django.contrib import admin
+from .models import Employee, Role, Department
+# Register your models here.
+
+
+
+admin.site.register(Employee)
+admin.site.register(Role)
+admin.site.register(Department)
